f25ba60ecd5cae6266cae7940c0180e744776419
cawoesies <155601989+cawoesies@users.noreply.github.com>
2024-03-15 10:59:52 +0700

    Add files via upload

---
+++ Kuliner.php
@@ -0,0 +1,251 @@
+<html>
+    <head>
+        <title>Kuliner</title>
+    </head>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li><a href="user_page.php">Home</a></li>
+                <li><a href="Kuliner.php">Kuliner</a></li>
+                <li><a href="Sejarah.php">Sejarah</a></li>
+                <li><a href="Near me.php">Near Me</a></li>
+            </ul>
+        </div>
+    </body>
+    <div class="jenis">
+        <h1>Kuliner</h1>
+          <div class="box-kiri" align="center">
+            <br>
+            <br>
+            <p><h3>Mie Aceh</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan nusantara khas Aceh ini punya tekstur yang tebal dan dicampur dengan irisan daging serta bumbu rempah yang kuat. Mie Aceh biasa disajikan dengan digoreng, direbus atau ditumis. Tentunya, bisa juga disajikan dengan tambahan seafood seperti kepiting, udang dan cumi.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/CT3xPidbAG1mSCM57" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+      
+          <div class="box-tengah" align="center">
+            <br>
+            <br>
+            <p><h3>Rendang</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan nusantara dari Sumatera Barat ini pernah dinobatkan sebagai makanan terlezat di dunia. Makanan dari daging sapi yang dimasak menggunakan santan kelapa serta bumbu rempah selama berjam-jam ini memang begitu terkenal. Rasanya yang gurih dan juga nikmat, sukses membuat makanan khas daerah Sumatera Barat ini begitu disukai banyak orang.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/X8dd3fDkazVW2CoHA" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+      
+          <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3>Gulai Belacan</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Gulai Belacan adalah masakan nusantara dari Riau. Belacan sendiri adalah nama lain dari terasi yang begitu dikenal oleh orang Sumatera. Makanan khas Riau ini dibuat dari bahan dasar kuah yang dicampur terasi. Di dalamnya ada isian berupa daging, udang atau ikan.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/ycWVed7UYFumJ27UA" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+      
+            <div class="box-kiri" align="center">
+              <br>
+              <br>
+              <p><h3>Bika Ambon</h3></p>
+              <br>
+              <hr>
+              <br>
+              <p>Meski namanya Bika Ambon, tapi masakan nusantara ini bukan berasal dari Ambon. Kue ini berasal dari Sumatera Utara dan banyak dijual di Kota Medan sebagai oleh-oleh. Kue berwarna kuning ini punya tekstur yang lembut dan manis.</p>
+              <br>
+              <br>
+              <a href="https://images.app.goo.gl/Mcmso4J9msjX1Psw6" class="cta-button"><button> Klik Disini</button></a>
+            </div>
+      
+            <div class="box-tengah" align="center">
+              <br>
+              <br>
+              <p><h3>Pempek</h3></p>
+              <br>
+              <hr>
+              <br>
+              <p>Makanan khas Sumatera Selatan ini sudah dikenal dengan kelezatannya. Masakan nusantara ini dibuat dari olahan ikan yang dicampur dengan sagu. Dilengkapi kuah cuko atau cuka yang begitu menggoda selera membuat banyak orang menggemarinya.</p> 
+              <br>
+              <br>
+              <a href="https://images.app.goo.gl/u3VWzqrFE1u45bMn9" class="cta-button"><button> Klik Disini</button></a>
+           </div>
+  
+           <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3>Kerak telor</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan nusantara ini sudah cukup terkenal, dan tentu sudah tak asing lagi. Kerak telor adalah salah satu makanan nusantara dari Jakarta yang selalu menggoda selera.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/BPdKeLHsewxWYtR56" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kiri" align="center">
+            <br>
+            <br>
+            <p><h3>Gudeg</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan nusantara dari Jogja wajib dicicipi. Gudeg adalah sayur nangka muda yang dimasak dengan santan tapi melalui proses yang lama. Gudeg biasa disajikan dengan nasi panas, kuah santan kental (areh), ayam kampung, telur dan serta sambal krecek.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/jAJbAocVFYd22Ybe9" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-tengah" align="center">
+            <br>
+            <br>
+            <p><h3>Rujak Cingur</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan nusantara dari Jawa Timur, yaitu rujak cingur yang sangat terkenal. Rujak dibuat dari campuran cingur (mulut sapi), sayur dan buah-buahan yang disirami dengan bumbu petis.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/gmXoF5jS5gBmwXQC6" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3>Ayam Betutu</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Ayam Betutu adalah masakan nusantara dari Bali. Betutu sendiri adalah lauk yang terbuat dari ayam atau bebek utuh berisi bumbu, lalu dipanggang dengan api sekam.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/eXXiLUFRmMttXSgJ6" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kiri" align="center">
+            <br>
+            <br>
+            <p><h3>Rawon</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Rawon merupakan masakan nusantara dari Jawa Timur yang paling terkenal. Makanan ini identik dengan kuah hitamnya. Warna hitam pada rawon tersebut tidak lain berasal dari bumbu khas yaitu kluwek. Rasa rawon sangat ramah bagi Anda yang tidak suka pedas. Masakan ini semakin nikmat disantap dengan nasi putih, kerupuk, serta lauk pelengkap seperti telur asin.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/Y9G7D8MdAtqnjfuw6" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-tengah" align="center">
+            <br>
+            <br>
+            <p><h3>Soto Banjar</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Soto Banjar adalah masakan nusantara dari daerah Kalimantan Selatan. Soto Banjar dibuat dari daging ayam yang dicampur dengan bumbu rempah-rempah, biasanya ditambah perkedel dan kentang rebus.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/9ybDgnykCujUJBd58" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3>Papeda</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Papeda merupakan salah satu masakan nusantara dari Papua yang dibuat dari sagu. Papeda harus dimasak selama beberapa menit hingga tekstur makanan berubah jadi bubur. Bubur putih dengan tekstur lengket seperti itu akan terasa hambar. Sebagai pelengkap, masakan nusantara ini diberi ikan kuah pedas dan sayur-sayuran.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/MhaDMxCpQMq4jgUq8" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kiri" align="center">
+            <br>
+            <br>
+            <p><h3>Gohu ikan</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Sedangkan di Maluku Utara, ada masakan nusantara yang khas yaitu Gohu Ikan. Masakan ini berbahan dasar ikan tuna mentah, yang dicampur dengan bumbu gohu.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/1o1CSuvX7fp74WBZ6" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-tengah" align="center">
+            <br>
+            <br>
+            <p><h3>Kepiting Soka</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Kepiting soka adalah makanan khas daerah Kalimantan Utara. Semua bagian kepiting ini bisa dimakan bahkan cangkangnya. Pasalnya, seluruh bagian tubuh kepiting soka ini sangat lunak.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/dAVbav9rLUghTc6D6" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3>Binte Biluhuta</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan ini khas dari daerah Gorontalo. Masakan ini dikenal dengan sebutan Milu Siram. Bahan utamanya dari jagung yang disiram dengan bumbu khas dan dicampur dengan udang, belimbing sayur dan juga topping pelengkap lainnya.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/jfjb9H6sb6mEromh8" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kiri" align="center">
+            <br>
+            <br>
+            <p><h3>Ayam Taliwang</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Masakan nusantara dari daerah Nusa Tenggara Barat ini berbahan dasar daging ayam. Daging ayam akan dibakar dan diberi bumbu cabai merah kering, bawang merah, bawang putih, tomat, terasi goreng, kencur, gula merah serta garam. Kemudian, ayam akan diberi bumbu tambahan, dan menu tambahan seperti plecing kangkung.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/udb3DhK5y4EdoZBX8" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-tengah" align="center">
+            <br>
+            <br>
+            <p><h3>Sop Konro</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Sop konro merupakan masakan nusantara dari daerah Sulawesi Selatan. Sup konro mirip sup iga sapi yang disajikan dengan kuah berwarna coklat.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/uhYogJ4J1tteNLaz8" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+
+          <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3>Serabi</h3></p>
+            <br>
+            <hr>
+            <br>
+            <p>Serabi adalah salah satu masakan nusantara dari Jawa Barat yang punya rasa manis. Dibuat dari tepung beras yang di panggang dan saat ini banyak dilengkapi aneka pilihan topping.</p>
+            <br>
+            <br>
+            <a href="https://images.app.goo.gl/J2uzafyv8EjoZxyK9" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+  
+</html>
---
+++ Sejarah.php
@@ -0,0 +1,95 @@
+<html>
+    <head>
+        <title>Sejarah</title>
+    </head>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li><a href="user_page.php">Home</a></li>
+                <li><a href="Kuliner.php">Kuliner</a></li>
+                <li><a href="Sejarah.php">Sejarah</a></li>
+                <li><a href="Near me.php">Near Me</a></li>
+            </ul>
+        </div>
+    </body>
+    <div class="jenis">
+        <h1>Sejarah</h1>
+          <div class="box-kiri" align="center">
+            <br>
+            <br>
+            <p><h3></h3></p>
+            <br>
+            <hr>
+            <br>
+            <p></p>
+            <br>
+            <br>
+            <a href="" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+      
+          <div class="box-tengah" align="center">
+            <br>
+            <br>
+            <p><h3></h3></p>
+            <br>
+            <hr>
+            <br>
+            <p></p>
+            <br>
+            <br>
+            <a href="" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+      
+          <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3></h3></p>
+            <br>
+            <hr>
+            <br>
+            <p></p>
+            <br>
+            <br>
+            <a href="" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+      
+            <div class="box-kiri" align="center">
+              <br>
+              <br>
+              <p><h3></h3></p>
+              <br>
+              <hr>
+              <br>
+              <p></p>
+              <br>
+              <br>
+              <a href="" class="cta-button"><button> Klik Disini</button></a>
+            </div>
+      
+            <div class="box-tengah" align="center">
+              <br>
+              <br>
+              <p><h3></h3></p>
+              <br>
+              <hr>
+              <br>
+              <p></p> 
+              <br>
+              <br>
+              <a href="" class="cta-button"><button> Klik Disini</button></a>
+           </div>
+  
+           <div class="box-kanan" align="center">
+            <br>
+            <br>
+            <p><h3></h3></p>
+            <br>
+            <hr>
+            <br>
+            <p></p>
+            <br>
+            <br>
+            <a href="" class="cta-button"><button> Klik Disini</button></a>
+          </div>
+  
+</html>
